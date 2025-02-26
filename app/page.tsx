483a55cc666183a5b8c6eab348bedd38f17feb93
Joel <joel.ruckstuhl@talents.jomb.ch>
2025-02-26 13:24:02 +0100

    v2.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
 import { NavBar } from "@/components/NavBar";
 import Image from "next/image";
 
-export default function header() {
-  return (      <>
-  <NavBar />
+export default function Header() {
+  return (
+    <>
+      <NavBar />
 
-      <main>
-
-        <section>
-          test
+      <main className="overflow-hidden">
+        <section className="flex justify-center items-start w-screen h-screen relative overflow-hidden">
+          <img
+            src="/illustration.svg"
+            className="w-full h-full object-cover"
+          ></img>
+        </section>
+        <section className="flex justify-center items-start w-screen h-screen relative overflow-hidden">
+          <div className="w-screen h-screen bg-white"></div>
         </section>
       </main>
-      </>        
+    </>
   );
 }
