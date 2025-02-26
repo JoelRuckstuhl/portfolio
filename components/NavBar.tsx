2029ed3c48c16ff6efe4a203d354dc70e5a05a8c
Joel <joel.ruckstuhl@talents.jomb.ch>
2025-02-26 09:13:40 +0100

    v1

---
+++ components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <header>
+        <div>
+testssrtstzehrdnrfsn
+        </div>
+        <nav>
+
+        </nav>
+    </header>
+  )
+}
